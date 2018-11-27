ccfad9af2628b8312b747ac8c6d4da75eb464635
tomohirokino <tomohiro.kino@d2cdot.co.jp>
2018-11-27 15:20:18 +0900

    fix webpack is not defined for prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: "production",
